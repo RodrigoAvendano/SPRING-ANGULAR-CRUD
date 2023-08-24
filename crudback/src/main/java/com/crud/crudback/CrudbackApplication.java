59b8d2c8e062f29cb82b75ace55fc264010458fc
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2023-08-24 16:20:53 -0600

    Feat: Add admin user

--- crudback/src/main/java/com/crud/crudback/CrudbackApplication.java
+++ crudback/src/main/java/com/crud/crudback/CrudbackApplication.java
@@ -1,16 +1,48 @@
 package com.crud.crudback;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.Set;
+
+import com.crud.crudback.entity.ERole;
+import com.crud.crudback.entity.RoleEntity;
+import com.crud.crudback.entity.UserEntity;
+import com.crud.crudback.repositories.UserRepository;
 
 @SpringBootApplication
 public class CrudbackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudbackApplication.class, args);
+	}
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Bean
+	CommandLineRunner init(){
+		return args -> {
+
+			UserEntity userEntity = UserEntity.builder()
+					.email("santiago@mail.com")
+					.username("santiago")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.ADMIN.name()))
+							.build()))
+					.build();
+			userRepository.save(userEntity);
+		};
 	}
 
 }
