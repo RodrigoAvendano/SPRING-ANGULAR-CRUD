19cd0afbff0ac95d8097db0b59bce1a36d1da36e
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2023-08-24 11:06:39 -0600

    Feat: Add Spring initilizr project

---
+++ SpringSecurityCRUD/src/test/java/com/crud/SpringSecurityCRUD/SpringSecurityCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.SpringSecurityCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
